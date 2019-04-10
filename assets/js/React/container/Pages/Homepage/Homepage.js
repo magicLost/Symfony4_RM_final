eecffd416ebc82a5e0c79e6e3036d8210d618ba2
Niko <oooze@mail.ru>
2019-04-10 17:39:52 +0300

    Add ICONS

--- assets/js/React/container/Pages/Homepage/Homepage.js
+++ assets/js/React/container/Pages/Homepage/Homepage.js
@@ -15,41 +15,7 @@ class Homepage extends React.PureComponent
         return (
         
             <div className={classes.Homepage}>
-                <h1>This is homePAGE mF...</h1>
-                <a href="http://public.local/large-print">Широкоформатная печать.</a>
 
-                <section className={classes.Test}>
-                    <div className={classes.TestItem}>
-                        <ControlsFeature
-                            items={[ 'Портфолио', 'Главное', 'Контакты' ]}
-                            itemClickHandler={() => {  }}
-                            formType={formType.BOTTOM_RIGHT_QUARTER}
-                            itemsLength={3}
-                            type={type.TEXT}
-                            isShowTitle={false}
-                        />
-                    </div>
-                    <div className={classes.TestItem}>
-                        <ControlsFeature
-                            items={[ 'Портфолио', 'Главное', 'Контакты' ]}
-                            itemClickHandler={() => {  }}
-                            formType={formType.BOTTOM_HALF_CIRCLE}
-                            itemsLength={3}
-                            type={type.TEXT}
-                            isShowTitle={true}
-                        />
-                    </div>
-                    <div className={classes.TestItem}>
-                        <ControlsFeature
-                            items={[ 'Портфолио', 'Главное', 'Контакты' ]}
-                            itemClickHandler={() => {  }}
-                            formType={formType.BOTTOM_RIGHT_QUARTER}
-                            itemsLength={3}
-                            type={type.TEXT}
-                            isShowTitle={false}
-                        />
-                    </div>
-                </section>
 
 
             </div>
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-//import './../style/style.scss';
+import './../style/style.scss';
 import Homepage from "./React/container/Pages/Homepage/Homepage";
 
 const mountNode =  document.getElementById('homepage_mount_node');
